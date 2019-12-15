bde3202c564e1085e4c3c3f2cd465b08f356a0ee
sunny sharma <SUNNY@DESKTOP-8L12CJI>
2019-12-15 18:50:04 +0530

    My project succesfully completed

---
+++ shoppingbackend/src/main/java/sunny/ss/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package sunny.ss.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
